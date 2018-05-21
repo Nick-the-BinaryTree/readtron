6c6fa1fa534c20906574aa81fa8cb8fe15604105
nick11h <nick11hd@gmail.com>
2018-05-21 19:46:17 -0400

    Possibly the beginning of an electron epub reader

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+const dialog = window.require('electron').remote.dialog;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.alterColor = this.alterColor.bind(this);
+    this.header = React.createRef();
+    this.button = React.createRef();
+  }
+
+  alterColor (light) {
+    if (this.header && this.button) {
+      if (light) {
+        this.header.current.classList.add("white");
+        this.button.current.classList.add("white");
+      } else {
+        this.header.current.classList.remove("white");
+        this.button.current.classList.remove("white");
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
+        <h1 id="header" ref={this.header}>Readtron</h1>
+        <div id="loadBook" ref={this.button}
+          onMouseEnter={() => this.alterColor(1)}
+          onMouseOut={() => this.alterColor(0)}
+          onClick={() => {
+            dialog.showOpenDialog(fileNames => {
+               if(fileNames === undefined) {
+                  console.log("No file selected");
+               } else {
+                  console.log("Sweet")
+               }
+            });
+          }}>
+          Load Book
         </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
       </div>
     );
   }
